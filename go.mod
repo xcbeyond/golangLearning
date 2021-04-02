a3127a45bf4b77d2a105a65a84f6a381dbaeaa7e
xcbeyond <xcbeyond157@163.com>
2021-04-02 14:24:01 +0800

    add & update

---
+++ go.mod
@@ -0,0 +1,10 @@
+module golangLearning
+
+go 1.16
+
+require (
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
+)
--- test.go
+++ test.go
@@ -3,38 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	sum := 0
-	for i := 1; i <= 100; i++ {
-		sum += i
-	}
-	fmt.Println(sum)
-
-	arr := []string{"A", "B", "C", "D"}
-	for k, v := range arr {
-		fmt.Printf("arr[%d]=%s\n", k, v)
-	}
-
-	var a int = 10
-	for a < 20 {
-		fmt.Printf("a=%d\n", a)
-		a++
-
-		if a > 15 {
-			continue
-		}
-		fmt.Printf("****\n")
-	}
-
-	var b int = 10
-	for b < 20 {
-		fmt.Printf("b=%d\n", b)
-		b++
-
-		if b > 15 {
-			goto print
-		}
-
-	}
 
 	// 不使用标记
 	fmt.Println("---- break ----")
@@ -56,8 +24,4 @@ re:
 			break re
 		}
 	}
-
-print:
-	fmt.Printf("****\n")
-
 }
